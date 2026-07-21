b31cabb9625f3996ee9278f24a7c0a0a7d3366df
YoungSeo-coder <ohyoungseo1783@gmail.com>
2026-07-21 20:54:33 +0900

    feat: 운영 사업장 2곳 추가 시드 마이그레이션(V10)

---
+++ src/main/resources/db/migration/V10__seed_additional_workspaces.sql
@@ -0,0 +1,4 @@
+INSERT INTO workspaces (name, address, category, district, latitude, longitude)
+VALUES
+    ('판타지아방탈출', '전남광주통합특별시 북구 호동로15번길 45 2층', '기타', '전대후문', 35.1772590, 126.9142990),
+    ('메가MGC커피 전대정문점', '전남광주통합특별시 북구 자미로 67 1층', '카페', '전대정문', 35.1719030, 126.9044980);
