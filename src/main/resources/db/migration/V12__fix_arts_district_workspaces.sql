65e38204ec96afe056cfe7feabf1151a755259b1
YoungSeo-coder <ohyoungseo1783@gmail.com>
2026-07-22 19:36:32 +0900

    fix: 예대 사업장 상권 정보 보정

---
+++ src/main/resources/db/migration/V12__fix_arts_district_workspaces.sql
@@ -0,0 +1,9 @@
+UPDATE workspaces
+SET district = '예대'
+WHERE name = '도토리베이커리 용봉점'
+  AND address = '전남광주통합특별시 북구 용주로30번길 47-3 1층';
+
+UPDATE workspaces
+SET district = '예대'
+WHERE name = '군함맥주 전남대점'
+  AND address = '전남광주통합특별시 북구 용주로30번길 78';
